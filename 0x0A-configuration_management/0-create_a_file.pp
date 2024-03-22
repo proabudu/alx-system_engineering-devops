e9c8cabd37ea0b1beec6819423e46443cd245b86
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-22 12:52:25 +0000

    correction of indentation in file 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
+# Create and manage a file at /tmp/school with specific content, ownership, and permissions.
 file { '/tmp/school':
-  ensure => present,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0744',
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
   content => 'I love Puppet',
 }
-
