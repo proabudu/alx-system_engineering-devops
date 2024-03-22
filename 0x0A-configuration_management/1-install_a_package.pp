08e867900361602811711481f80831fc76f1a541
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-22 13:17:30 +0000

    e

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # installing flask using pip3
 python::pip { 'flask':
-  ensure    => '2.1.0',
-  pkgname   => 'flask',
-  provider  => 'pip3',
+  ensure   => '2.1.0',
+  pkgname  => 'flask',
+  provider => 'pip3',
 }
