7bc9a33ebb33152568aaacefc114018093ece561
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-27 01:30:42 +0000

    assd

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# Using Puppet, install flask from pip3.
-package
+# Ensure pip3 is installed before trying to install Flask
 
-{ 'flask':
- ensure  =>  '2.1.0',
- provider =>  'pip3',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
