50d02d7c6d6ff82195cee242d242ae4c6b2ac584
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-27 01:20:53 +0000

    the code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-# Using Puppet, install flask from pip3.
-package
-{ 'flask':
-  ensure   =>   '2.1.0',
-  provider =>   'pip3',
+# Include the python class with ez_setup enabled
+class { 'python': ez_setup => true, }
+
+# Use pip provider to install Flask 2.1.0
+package { 'flask':
+  ensure  => '2.1.0',
+  provider => 'pip3',
 }
