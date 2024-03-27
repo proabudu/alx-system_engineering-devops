2ac6baab3df4c5df579872dd4406710c375eb8c9
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-27 01:34:14 +0000

    mmm

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Ensure pip3 is installed before trying to install Flask
 
-package { 'flask':
+package
+{ 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
